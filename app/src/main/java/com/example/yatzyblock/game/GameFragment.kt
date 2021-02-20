d5ea561d2faf8add136ab869d24af5badcfde9e2
Marvin Moosbacher <moosbacher94@gmail.com>
2021-02-20 14:45:06 +0100

    Started with tablelayout implementation

--- app/src/main/java/com/example/yatzyblock/game/GameFragment.kt
+++ app/src/main/java/com/example/yatzyblock/game/GameFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TableRow
 import android.widget.TextView
 import com.example.yatzyblock.R
@@ -36,13 +37,33 @@ class GameFragment : Fragment(R.layout.fragment_game) {
 
 
     private fun buildTableLayout(players: Array<Player>) {
-        for(player in players) {
-            val row = TableRow(context)
-            val textViewName = TextView(context)
-            textViewName.text = player.name
-            row.addView(textViewName)
-            binding.yatzyTableLayout.addView(row)
+
+        val listRows: MutableList<TableRow> = mutableListOf()
+
+        var i = 0
+        while (i < 18) {
+            val tableRow = TableRow(context)
+            listRows.add(tableRow)
+            val firstColumnTextView = TextView(context)
+            firstColumnTextView.text = "name$i"
+            tableRow.addView(firstColumnTextView)
+            binding.yatzyTableLayout.addView(tableRow)
+            i++
         }
+
+        listRows.forEachIndexed { indexRows, row ->
+            players.forEachIndexed { indexPlayers, player ->
+                if (indexRows == 0) {
+                    val nameTextView = TextView(context)
+                    nameTextView.text = player.name
+                    row.addView(nameTextView)
+                } else {
+                    val editTextView = EditText(context)
+                    row.addView(editTextView)
+                }
+            }
+        }
+
     }
 
 }
